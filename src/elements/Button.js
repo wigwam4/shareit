6cc83dde7e33f0f147a0d2b66e22fefbe15ff50d
조현홍 <wigwam4@naver.com>
2022-12-21 21:16:06 +0900

    <feat> 버튼 추가

---
+++ src/elements/Button.js
@@ -0,0 +1,33 @@
+import styled, { css } from 'styled-components';
+
+let Button = styled.button`
+    display: inline-flex; 
+    justify-content: center; align-items: center;
+    border-width: 1px;
+    border-style: solid;
+    border-radius: 4px;
+    outline: none;
+
+    ${props => props.primary && css`
+        color: #fff;
+        background: #0077ED;
+        border-color: #0077ED;
+    `}
+    ${props => props.secondary && css`
+        color: #677294;
+        background: #fff;
+        border-color: #d8dee5;
+    `}
+    ${props => props.lg && css`
+        padding: 0 16px;
+        width: 204px; height: 50px;     
+        font-size: 1.8rem; line-height: 1.444em;
+    `}
+    ${props => props.sm && css`
+        padding: 0 11px;
+        height: 30px;
+        font-size: 1.3rem; font-weight: 500; line-height: 1.461em;
+    `}
+`
+
+export { Button };
