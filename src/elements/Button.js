1cde3f615e2028ab12be153a8bea14eedffd6785
조현홍 <wigwam4@naver.com>
2022-12-22 01:02:19 +0900

    <style> 버튼 hover 스타일 추가

--- src/elements/Button.js
+++ src/elements/Button.js
@@ -6,17 +6,27 @@ const Button = styled.button`
     border-width: 1px;
     border-style: solid;
     border-radius: 4px;
+    transition: 0.3s;
+    cursor: pointer;
     outline: none;
 
     ${props => props.primary && css`
         color: #fff;
         background: #0077ED;
         border-color: #0077ED;
+        &:hover {
+            background: #0064ed;
+            border-color: #0064ed;
+        }
     `}
     ${props => props.secondary && css`
         color: #677294;
         background: #fff;
         border-color: #d8dee5;
+        &:hover {
+            background: #f8f8f8;
+            border-color: #f8f8f8;
+        }
     `}
     ${props => props.lg && css`
         padding: 0 16px;
